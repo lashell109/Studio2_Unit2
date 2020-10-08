55a0202ee7096d3257542b4de0f41132afe673d4
La Shell Slaughter <lashellslaughter109@gmail.com>
2020-10-08 15:51:22 -0500

    Added code

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Studio2_Unit2.CountingCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string poem;
+            Console.WriteLine("Please enter a string:");
+            poem = Console.ReadLine().ToLower(); 
+            Console.WriteLine("Here is your character count:");
+            while (poem.Length > 0)
+            {
+                Console.Write(poem[0] + "=");
+                int letters = 0;
+                for (int j = 0; j < poem.Length; j++)
+                {
+                    if (poem[0] == poem[j])
+                    {
+                        letters++;
+                    }
+                }
+                Console.WriteLine(letters);
+                poem = poem.Replace(poem[0].ToString(), string.Empty);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
